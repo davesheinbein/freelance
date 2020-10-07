4edd72ca65b00904d27a1816d2bd4f0264c67747
David Sheinbein <davesheinbein@gmail.com>
2020-10-07 16:39:23 -0700

    update to env and server

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const logger = require('morgan');
 const favicon = require('serve-favicon');
 const cors = require('cors');
 const sgMail = require('@sendgrid/mail');
-
 const app = express();
 
-require('dotenv').config('.sendgrid.env');
+require('dotenv').config();
+require('dotenv').config({ path: '.sendgrid.env' })
 
 app.use(cors());
 app.use(logger('dev'));
@@ -16,28 +16,23 @@ app.use(
 	favicon(path.join(__dirname, 'build', 'favicon.ico'))
 );
 app.use(express.static(path.join(__dirname, 'build')));
-
 // SendGrid Key
 const sendGridKey = process.env.SENDGRID_API_KEY;
 sgMail.setApiKey(sendGridKey);
-
 // Welcome page of the express server:
 app.get('/', (req, res) => {
 	res.send('Welcome to the Sendgrid Emailing Server');
 });
-
 // Email Config
 app.get('/send-email', (req, res) => {
 	// Get Variable query string
 	const { recipient, sender, topic, text } = req.query;
-
 	const msg = {
 		to: recipient,
 		from: sender,
 		subject: topic,
 		text: text,
 	};
-
 	// send Email
 	sgMail
 		.send(msg)
@@ -45,11 +40,9 @@ app.get('/send-email', (req, res) => {
 			console.log(msg, '<<< msg', text, '<<< text')
 		);
 });
-
 app.get('/*', function (req, res) {
 	res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-
 const port = process.env.PORT || 3001;
 app.listen(port, function () {
 	console.log(`Express app listening on port ${port}`);
